68e35e268c0ab0e201894cdb0f69b2f9e5e7a9b4
Mayder Cortés <maydercortes1@gmail.com>
2020-12-14 13:29:21 -0600

    update responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 
+/* Cargamos los svg tras la carga del DOM */
+document.addEventListener('DOMContentLoaded', () => {
+    /* Buscamos cada elemento svg con el atributo "src" */
+    document.querySelectorAll('svg[data-src]').forEach(svg => {
+      /* Cargamos el contenido en su HTML interno */
+      fetch(svg.dataset.src)
+        .then(respuesta => respuesta.text())
+        .then(xml => svg.innerHTML = xml);
+    });
+  });
+
 document.getElementById("title").onmouseover = ()=>{
     
     this.title.classList.add("animate__bounce");
@@ -14,7 +25,11 @@ document.getElementById("title").onmouseover = ()=>{
 }
 
 document.getElementById("nav__btn").onclick = function(){
+
     this.classList.toggle("btn_animation");
+
+
+
     let nav_lista = document.getElementById("nav_res");
     let home =  document.getElementById("home_li");
 
@@ -33,5 +48,21 @@ document.getElementById("nav__btn").onclick = function(){
     console.log(contact)
 
 }
+
+//Simular click a boton de menu maximizar o pasar media querie
+
+let mql = window.matchMedia("(min-width: 872px) and (max-width: 890)")
+// no se si hay que ponerle evento vamos aprobar
+function media_resize(){
+    let lista_clases = document.getElementsByClassName("nav__lista__item");
+    console.log(lista_clases[2].classList["value"])
+}
+
+
+media_resize()
+window.onresize = ()=>{
+    media_resize()
+}
+
 
 
