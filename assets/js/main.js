d0410b680393722d2406e03edfc69e8302c25b3f
Mayder Cortés <maydercortes1@gmail.com>
2020-12-14 17:21:52 -0600

    fix boton responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,26 @@
 
-/* Cargamos los svg tras la carga del DOM */
-document.addEventListener('DOMContentLoaded', () => {
-    /* Buscamos cada elemento svg con el atributo "src" */
-    document.querySelectorAll('svg[data-src]').forEach(svg => {
-      /* Cargamos el contenido en su HTML interno */
-      fetch(svg.dataset.src)
-        .then(respuesta => respuesta.text())
-        .then(xml => svg.innerHTML = xml);
-    });
-  });
-
 document.getElementById("title").onmouseover = ()=>{
     
     this.title.classList.add("animate__bounce");
     this.title.classList.add("animate__animated");
 
-    console.log(this.title.classList);
-
     setTimeout(()=>{
         this.title.classList.remove("animate__bounce");
         this.title.classList.remove("animate__animated");
     }, 1000)
-
 }
+
 
 document.getElementById("nav__btn").onclick = function(){
 
     this.classList.toggle("btn_animation");
 
+    let bandera_btn = this.classList.contains("btn_animation")
+    console.log(bandera_btn)
+
+    if(bandera_btn == false && window.matchMedia("(min-width: 872px)").matches){
+        this.style.visibility="";
+    }
 
 
     let nav_lista = document.getElementById("nav_res");
@@ -45,24 +38,24 @@ document.getElementById("nav__btn").onclick = function(){
 
     let contact = document.getElementById("btn_contact");
     contact.classList.toggle("item__contact2")
-    console.log(contact)
 
 }
 
 //Simular click a boton de menu maximizar o pasar media querie
-
-let mql = window.matchMedia("(min-width: 872px) and (max-width: 890)")
+// todo arriba de 872 y bajo 890
+let mql = window.matchMedia("(min-width: 872px) and (max-width: 880)")
 // no se si hay que ponerle evento vamos aprobar
 function media_resize(){
     let lista_clases = document.getElementsByClassName("nav__lista__item");
-    console.log(lista_clases[2].classList["value"])
+    let bandera = lista_clases[2].classList.contains("nav__lista__item2")
+    if(bandera == true){
+        //cuando desaparece es true i entra al if para volverle a aplicar vivibility en boton,
+        document.getElementById("nav__btn").style.visibility = "visible";
 }
 
+
+window.onresize = ()=>
+    media_resize();
+}
 
 media_resize()
-window.onresize = ()=>{
-    media_resize()
-}
-
-
-
