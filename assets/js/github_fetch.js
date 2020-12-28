6cae18875ad81ba4fb4c93b1d9c047f8df9f5d74
MayderC <maydercortes1@gmail.com>
2020-12-27 23:50:19 -0600

    footer and body

--- assets/js/github_fetch.js
+++ assets/js/github_fetch.js
@@ -14,9 +14,21 @@ class Consulta{
             .then( data => {
                 //metodos que muestran info de la api en el dom
                 this.printAvatar(data)
-                this.reposCantidad(data)
+                console.log(data)
             }).catch( error => console.error(error))
     }
+
+    getRepos(){
+        fetch("https://api.github.com/users/MayderC/repos")
+        .then(response =>response.json())
+        .then( data => {
+            console.log(data)
+        }).catch( error => {
+            console.error(error)
+        })
+    }
+
+
 
     printAvatar(data){
         console.log(this.id_avatar_dom)
@@ -24,12 +36,7 @@ class Consulta{
         document.getElementById("id_name_github").innerHTML = `${data.login}`
     }
 
-    reposCantidad(data){
-        document.getElementById("repos").innerHTML =`${data.public_repos}<br> Repositories`
-        console.log(data.public_repos)
-    }
 }
 
 //let consulta = new Consulta("img_avatar")
 //consulta.getData("https://api.github.com/users/MayderC")
-    
