262f3e9bacfdb71d801754cfc552f705eead3d0f
MayderC <maydercortes1@gmail.com>
2021-05-07 00:51:30 -0600

    add github repos, api

--- assets/js/github_fetch.js
+++ assets/js/github_fetch.js
@@ -1,71 +1,170 @@
-class Consulta{
+class Consulta {
+	constructor() {
+		// elementos del dom
 
-    constructor(id_avatar_dom){
-        // elementos del dom
-        this.id_avatar_dom = id_avatar_dom;
+		// lista o array de repositorios
+		this.repositorios = [];
+	}
+	// Metodo que hace peticion a api.
 
-        // lista o array de repositorios
-        this.repositorios = []
-    }
-    // Metodo que hace peticion a api.
-    getData(url){
-        fetch(url)
-            .then( response => response.json())
-            .then( data => {
-                this.printAvatar(data)
+	getData(uri, callback) {
+		fetch(uri)
+			.then((response) => response.json())
+			.then((data) => {
+				this.repositorios = data;
 
-            }).catch( error => console.error(error))
-    }
+				callback(null, this.repositorios);
+			})
+			.catch((error) => {
+				let error_api = new Error(error);
+				return callback(error_api, null);
+			});
+	}
+}
+//Estructura de URL para lenguajes usados y su porcentaje
+//https://api.github.com/repos/MayderC/App_contactos/languages
 
-    getRepos(){
-        fetch("https://api.github.com/users/MayderC/repos")
-        .then(response =>response.json())
-        .then( data => {
-            console.log(data)
-        }).catch( error => {
-            console.error(error)
-        })
-    }
+let consulta = new Consulta();
+ let uri = "https://api.github.com/users/MayderC/repos"
+consulta.getData(
+	uri,
+	(error, response) => {
+		if (error) { return; }
 
-    printRepositories(data, config){
-        
-
-    }
-
+		let namesOfRepoAndRepo = [];
 
 
-    printAvatar(data){
-        console.log(this.id_avatar_dom)
-        document.getElementById(this.id_avatar_dom).innerHTML = `<img class="container__img" src="${data.avatar_url}" alt="avatar_github">`
-        document.getElementById("id_name_github").innerHTML = `${data.login}`
-    }
+		for (let i in response) {
+			if (response[i].name == undefined) { return; }
+
+			
+			let nombre = `https://api.github.com/repos/MayderC/${response[i].name}/languages`
+			//let nombre = `http://127.0.0.1:5501/localdata/${response[i].name}.json`;
+
+			if (
+				response[i].name != "MayderC" &&
+				response[i].name != "MayderC.github.io"&&
+				response[i].name != "uptask"
+			) {
+				consulta.getData(nombre, (error2, response2) => {
+					if (error2) {
+						return;
+					}
+
+					namesOfRepoAndRepo.push({"name": response[i].name, "data": response2})
+
+					if(i == response.length-1){
+						prepareChart(namesOfRepoAndRepo);
+					}
+				});
+			}
+		}
+	}
+);
+
+function prepareChart(data) {
+
+	let labels = [];
+	let dataValue = [];
+	let globalArray = []
+
+	for(let i = 0; i < data.length; i++ ){
+
+		labels.push(Object.keys(data[i].data))
+		dataValue.push(Object.values(data[i].data))
+	}
+
+
+	let newColors = []
+	
+	for(let i =0; i < labels.length; i++){
+		
+		newColors = getColorFrom(labels.length);
+		generateElementDom(data[i].name);
+		
+	}
+	for(let i =0; i < labels.length; i++){
+		
+		printChart(labels[i], dataValue[i], newColors, data[i].name)
+	}
+
+
+
+	console.log(arrayChart)
+
 
 }
 
-//let consulta = new Consulta("img_avatar")
-//consulta.getData("https://api.github.com/users/MayderC")
-var ctx = document.getElementById('myChart');
-Chart.defaults.font.size = 16;
-Chart.defaults.color= "white"
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Html', 'css', 'Js'],
-        datasets: [{
-            data: [ 130, 700, 300],
+let arrayChart = []
+function printChart(labels, data, colors, id){
 
-            backgroundColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)'
-            ],
-            borderWidth: 3
-        }],
+	Chart.defaults.font.size = 16;
+	Chart.defaults.color = "black";
 
-    }
-});
+
+		new Chart(document.getElementById(id), {
+			type: "pie",
+			data: {
+				labels: labels,
+				datasets: [
+					{
+						data: data,
+						backgroundColor: colors,
+						borderWidth: 5,
+					},
+				],
+			},
+		})
+	
+
+}
+
+function getColorFrom(n) {
+	const colors = [
+		"#92F0A1",
+		"#585186",
+		"#F02996",
+		"purple",
+		"#7DF029",
+		"#29EDF0",
+		"#2956F0",
+		"#59B7D5",
+		"#15657F",
+	];
+
+	return colors.slice(0, n);
+}
+
+function generateElementDom(name) {
+	const cardContainer = document.getElementById("card_container");
+
+	let htmlString = ``;
+
+		htmlString = `
+		<div class="proyects__item">
+
+			<div class="proyects__header">
+
+				<div class="proyects__img">
+					<img src="https://avatars.githubusercontent.com/u/44930667?v=4" alt="">
+				</div>
+				<div class="header__text--user">
+					<p>MayderC</p>
+				</div>
+				<div class="header__text--title_repo">
+				</div>
+				
+				</div>
+				<div class="proyects__body">
+				<p>${name}</p>
+					<div class="chart-container chart-container">
+						<canvas id="${name}" width="400" height="400"></canvas>
+					</div>
+			</div>
+
+		</div>
+		`;
+	
+	cardContainer.innerHTML += htmlString;
+	console.log(htmlString)
+}
