f4b79da9bdedc4cd74d317a754a91082656485c5
MayderC <maydercortes1@gmail.com>
2021-06-18 20:03:23 -0600

    add menu-color-dinamic and change theme-color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-
+let isTranparent = document.getElementById('nav').classList.contains('nav__tranparent-scrolly')
+let isColor = document.getElementById('nav').classList.contains('nav__color-scrolly')
 // EVENTO CLICK BOTON RESPONSIVE  ALICACION Y ELIMINACION DE ESTILOS
 var boton_responsive = false;
 
@@ -8,10 +9,20 @@ var sc = []
 sc[0] = window.scrollY
 var y = 0;
 
+let nav_lista = document.getElementById("nav_res");
+let home = document.getElementById("home_li");
+
+let isMenuClose = true;
+
 
 document.getElementById("nav__btn").onclick = function () {
     //Aañade o quita animacion de menu en cada click. abre o cierra
     this.classList.toggle("btn_animation");
+
+    isMenuClose = !isMenuClose;
+
+
+
 
 
     // comprueba si esta abierto o cerrado el menu, boton menu.
@@ -25,13 +36,19 @@ document.getElementById("nav__btn").onclick = function () {
 }
 
 // Todos los estilos que se deben quitar o añadir al dar click en boton responsive
+
+
 function estilosMenuResponsive() {
 
-    let nav_lista = document.getElementById("nav_res");
-    let home = document.getElementById("home_li");
+    if (nav_lista.parentNode.classList.contains('nav__color-scrolly') && isColor) {
+
+        nav_lista.classList.toggle('nav3')
+    } else {
+
+        nav_lista.classList.toggle("nav2");
+    }
 
     nav_lista.classList.toggle("nav__lista2")
-    nav_lista.classList.toggle("nav2");
     home.classList.toggle("item__home2")
 
 
@@ -66,6 +83,7 @@ y las activa cuando se desactiva los estilos del menu responsive. y la ventana e
 */
 function media_resize() {
 
+
     let med = window.matchMedia("(min-width: 872px)");
     let btn = document.getElementById("nav__btn");
     let estilo_responsive = btn.classList.value.split(" ").includes("btn_animation")
@@ -93,11 +111,43 @@ function animacion_scroll() {
     if (pos_animacion < pantalla) {
 
         animacion.classList.add("flip-img");
-    } else if (pos_animacion > pantalla+50) {
+    } else if (pos_animacion > pantalla + 50) {
         animacion.classList.remove("flip-img");
 
     }
 }
+
+//cambia el estilo de la barra de navegacion, cerca o lejos del top.
+
+function changeStyleNavOnScroll() {
+
+    isTranparent = document.getElementById('nav').classList.contains('nav__tranparent-scrolly')
+    isColor = document.getElementById('nav').classList.contains('nav__color-scrolly')
+    //Comprueba si es tranparente o tiene color. para evitar entrar a los if. con scroll
+
+    // mayor a 175 Color
+    // Menor a 175 estilo Transparente
+    let items_menu = [...document.querySelectorAll('.item__link')]
+
+    if (window.scrollY > 175 && !isColor && isMenuClose) {
+
+        document.getElementsByClassName('link__contact')[0].style.color = "white"
+        document.getElementById('btn_contact').style.backgroundColor = ""
+        document.getElementById('nav').classList.remove('nav__tranparent-scrolly')
+        document.getElementById('nav').classList.add('nav__color-scrolly')
+
+    } else if (window.scrollY < 176 && !isTranparent && isMenuClose) {
+
+
+        document.getElementById('btn_contact').style.backgroundColor = "white"
+        document.getElementById('nav').classList.remove('nav__color-scrolly')
+        document.getElementById('nav').classList.add('nav__tranparent-scrolly')
+        document.getElementsByClassName('link__contact')[0].style.color = "rgb(36, 36, 36)"
+
+    }
+}
+
+
 
 function autoHide() {
     y = window.scrollY;
@@ -109,17 +159,15 @@ function autoHide() {
 
         //console.log("Lo oculto")
 
-    }else if(sc[0] > sc[1]+20){
+    } else if (sc[0] > sc[1] + 20) {
         nav.classList.remove("hidden")
-
-
         //console.log("Lo muestro", sc[0], sc[1])
         sc[0] = sc[1]
     }
-
     if (sc[0] < 10) {
-       // nav.style.position = "relative"
+        // nav.style.position = "relative"
     } else {
+
         nav.style.position = "fixed"
     }
 }
@@ -131,7 +179,7 @@ media_resize()
 window.onscroll = () => {
     animacion_scroll();
     autoHide();
-
+    changeStyleNavOnScroll()
 }
 
 
