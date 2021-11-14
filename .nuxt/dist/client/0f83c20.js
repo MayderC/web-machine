93fb37e3f2b89cd49d87ece2265820c0df4c6c37
MayderC <mayydercortes1@gmail.com>
2021-11-14 10:39:35 -0600

    make it better

--- .nuxt/dist/client/0f83c20.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{415:function(e,n,t){"use strict";t.r(n);var l=t(35),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Mayder")])}),[],!1,null,null,null);n.default=component.exports}}]);
